1647b5e81610917250ab74d303bd736d9e038a9f
Wileybot2004 <72325805+Wileybot2004@users.noreply.github.com>
2023-01-31 15:06:46 -0500

    Add files via upload

--- TinkersCalc.py
+++ TinkersCalc.py
@@ -1,11 +1,11 @@
 from tkinter import *
 window = Tk()
-window.title("Tinker's Construct smeltery resource calcuator")
+window.title("Tinker's Construct Smeltery Resource Calcuator")
 
 
 header = Label(text="Tinkers Construct Smeltery Resouce Calcuator", font="bold")
 header.pack()
-direction = Label(text="Please fill out all options then click the Calcuate button to figure out how many resources you need to create a smeltery of that size.")
+direction = Label(text="Please fill out all options then click the Calcuate button below to figure out how many resources you need to create a smeltery of that size.")
 direction.pack()
 
 
@@ -33,7 +33,7 @@ def action():
     tmp3 = tmp2 + 6
     tmp4 = tmp3 * 4
     tmp5 = tmp4 + 8 + 6 + 8 + 3 + 7
-    output.config(text=f"You need {tmp5} Seared brick(item) and 1 Glass for {tmp3} Seared brick blocks, 1 smeltery controller, 1 seared tank, 1 seared drain, 1 seared faucet and 1 casting table")
+    output.config(text=f"You need {tmp5} Seared brick(item) and 1 Glass for {tmp3} Seared brick blocks, 1 Smeltery Controller, 1 Seared Tank, 1 Seared Drain, 1 Seared Faucet and 1 Casting Table")
 
 
         
