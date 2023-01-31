9b824406b22aaed86afa2b0a14596b80f0816bfd
Wileybot2004 <72325805+Wileybot2004@users.noreply.github.com>
2023-01-31 14:27:50 -0500

    Add files via upload

---
+++ TinkersCalc.py
@@ -0,0 +1,60 @@
+from tkinter import *
+window = Tk()
+window.title("Tinker's Construct smeltery resource calcuator")
+
+
+header = Label(text="Tinkers Construct Smeltery Resouce Calcuator", font="bold")
+header.pack()
+direction = Label(text="Please fill out all options then click the Calcuate button to figure out how many resources you need to create a smeltery of that size.")
+direction.pack()
+
+
+#size selector
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="3x3 Smeltery", value=3, variable=radio_state)
+radiobutton2 = Radiobutton(text="5x5 Smeltery", value=5, variable=radio_state)
+radiobutton3 = Radiobutton(text="7x7 Smeltery", value=7, variable=radio_state)
+radiobutton1.pack()
+radiobutton2.pack()
+radiobutton3.pack()
+
+
+#height selector
+spinbox = Spinbox(from_=1, to=5, width=5)
+spinbox.pack()
+#main logic
+def action():
+    size = radio_state.get()
+    height = spinbox.get()
+    int(size)
+    heightint = int(height)
+    tmp = size * heightint
+    tmp2 = tmp * 4
+    tmp3 = tmp2 + 6
+    tmp4 = tmp3 * 4
+    tmp5 = tmp4 + 8 + 6 + 8 + 3 + 7
+    output.config(text=f"You need {tmp5} Seared brick(item) and 1 Glass for {tmp3} Seared brick blocks, 1 smeltery controller, 1 seared tank, 1 seared drain, 1 seared faucet and 1 casting table")
+
+
+        
+
+
+
+
+
+
+
+button = Button(text="Calculate", command=action)
+button.pack()
+
+
+output = Label(text="")
+output.pack()
+
+
+
+
+
+
+
+window.mainloop()
